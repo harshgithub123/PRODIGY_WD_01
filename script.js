f7a98fbd1186c4c5ba812b257203c0c510afb9f7
harshgithub123 <harshkumar02101998@gmail.com>
2024-05-13 10:58:39 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("scroll", function() {
-  const navbar = document.getElementById("navbar");
-  if (window.scrollY > 50) {
-    navbar.classList.add("scrolled");
-  } else {
-    navbar.classList.remove("scrolled");
-  }
-});
-
